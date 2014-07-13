82157af936c998473a8a34662937583a0171c50f
John Flackett <john@koolth.com.au>
2014-07-13 17:06:19 +0930

    Updated main project webpages.

--- lrs/index.php
+++ lrs/index.php
@@ -11,18 +11,22 @@ include '../includes/headerHtml.php';
 	<?php include 'header.html'; ?>
 
 	<div class="content-wrapper">
-		<div class="content pure-g">
-			<h2 class="content-head is-center">Introduction</h2>
+		<div class="content">
 
-		    <div class="pure-u-1-2 u-sm-2-3 u-md-1-2">
-		        <img src="http://placehold.it/505x310" class="pure-img-responsive" alt="LRS.">
-		    </div>
+			<h2 class="content-head pure-u-1-2">Introduction</h2>
 
-		    <div class="content pure-u-1-2 u-md-1-2">
+		    <div class="content pure-u-2-2">
 		        <h3 class="lrs-header">Overview.</h3>
-		        <p>
-		            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus maiores et impedit odit similique nostrum pariatur, cum asperiores nisi minus ea repellat quidem, quam consequatur sunt officia eaque iusto ad.
-		        </p>
+		        <p>The well-being of all life on this planet is under increasing pressure from the reality of climate change.  It has been known for decades that part of the solution lies in repairing our natural environment through tree planting activities and restoring forest to areas that have been cleared.</p>
+		        <p>This is where our creation, the “What Grow’s Here?” app can help.  Our app will enable any person from students to professionals to become empowered and be part of the solution to plant the right tree, at the right time in the right place. The “What Grow’s Here?” app uses data from state departments to select plants that are not only native, but indigenous to a location and also shows what the plant looks like growing in its natural environment.  The information available on the app goes further to include what animals are dependent on the tree, ecosystem benefits derived from the tree (e.g.: oxygen created, carbon sequestered), observations that humans have made about that tree through citizen science contributions (eg: folk lore, nesting birds, limb fall, sightings) and an ability to link to an online store to purchase chosen plants from the State Flora Nursery.</p>
+		        <p>The importance of using plants that are not only Australian natives, but indigenous to a location is outlined below:</p>
+		        <ul>
+		        	<li><b>Better survival mechanisms</b> - The indigenous plants of an area have evolved over a long period of time to suit local conditions (climate, rainfall and soil) meaning that they have greater survival rates and no reliance on chemicals for plant health.</li>
+		        	<li><b>Ecological Balance</b> - A healthy ecosystem means that there is strong interdependence between the plants and the native wildlife they have evolved with. For example, many plants and animals are closely linked in their pollination and breeding cycles and are dependent on each other for survival.</li>
+		        	<li><b>Pest and disease control</b> - Local plant species provide habitat for local animals and therefore a system of natural pest control is in place which keeps parasites and predators in balance.
+		        	</li>
+		        	<li><b>Educational tool</b> - Showing the community how to plant out using indigenous plants will help them better appreciate their local environment and recognise the benefits that a vibrant, functioning ecosystem can deliver.</li>
+		        </ul>
 
 		    </div>
 
