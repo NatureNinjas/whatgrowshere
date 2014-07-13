c8001977289cf80f24b5a164ebf08dddcc1f85a4
John Flackett <john@koolth.com.au>
2014-07-13 13:51:48 +0930

    Created HTML template 'includes' files (e.g., header/footer). Used in LMS system.

---
+++ includes/headerHtml.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+	<!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title><?php echo isset($pageTitle)?$pageTitle:"Nature Ninjas"; ?></title>
+	<link rel="stylesheet" href="../css/normalize.min.css">
+	<link rel="stylesheet" href="../css/main.css">
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+		<!--[if lte IE 8]>
+		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/grids-responsive-old-ie-min.css">
+		<![endif]-->
+		<!--[if gt IE 8]><!-->
+		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/grids-responsive-min.css">
+		<!--<![endif]-->
+		<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+		<script src="../js/vendor/modernizr-2.6.2.min.js"></script>
+</head>
--- lms/index.php
+++ lms/index.php
@@ -1,26 +1,8 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html class="no-js">
-	<!--<![endif]-->
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Nature Ninjas | LMS</title>
-	<link rel="stylesheet" href="../css/normalize.min.css">
-	<link rel="stylesheet" href="../css/main.css">
-	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
-		<!--[if lte IE 8]>
-		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/grids-responsive-old-ie-min.css">
-		<![endif]-->
-		<!--[if gt IE 8]><!-->
-		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/grids-responsive-min.css">
-		<!--<![endif]-->
-		<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-		<script src="../js/vendor/modernizr-2.6.2.min.js"></script>
-</head>
+<?php 
+$pageTitle = "Nature Ninjas | LMS";
+include '../includes/headerHtml.php'; 
+?>
+
 <body id="learn">
 	<!--[if lt IE 7]>
 		<p class="browsehappy">Your browser is slightly outdated. You can <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
@@ -29,17 +11,14 @@
 	<div class="header">
 		<div class="home-menu pure-menu pure-menu-open pure-menu-horizontal pure-menu-fixed">
 			<a class="pure-menu-heading brand" href="">
-				Learning <span>Management</span> System!
+				Learning <span>Resource</span> System!
 			</a>
 				<ul>
 					<li class="pure-menu-selected">
 						<a href="index.php">Home</a>
 					</li>
 					<li>
-						<a href="#">Course</a>
-					</li>
-					<li>
-						<a href="#">Example</a>
+						<a href="example.php">Example</a>
 					</li>
 				</ul>
 		</div>
@@ -47,17 +26,8 @@
 
 	<div class="content-wrapper">
 		<div class="content">
-			<h2 class="content-head is-center">Header</h2>
+			<h2 class="content-head is-center">Introduction</h2>
 		</div> <!-- .content -->
 	</div> <!-- .content -->
 
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script>
-		window.jQuery || document.write('<script src="../js/vendor/jquery-1.11.0.min.js"><\/script>')
-	</script>
-	
-	<script src="../js/plugins.js"></script>
-	<script src="../js/main.js"></script>
-
-</body>
-</html>
+<?php include '../includes/footerHtml.php';  ?>
